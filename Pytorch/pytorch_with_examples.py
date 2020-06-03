bd61ddca3fddcffa1ffdb2eb38330880f89b732a
JSVinasco <vinasco.juan@correounivalle.edu.co>
2020-06-03 11:02:21 -0500

    cambios pytorch

---
+++ Pytorch/pytorch_with_examples.py
@@ -0,0 +1,14 @@
+#########################################
+###     Pytorch with Exampes Tutorial  ###
+#########################################
+
+# This tutorial introduces the fundamental concepts of PyTorch through self-contained examples.
+# At its core, PyTorch provides two main features:
+# -An n-dimensional Tensor, similar to numpy but can run on GPUs
+# -Automatic differentiation for building and training neural networks
+
+
+# import the libraries
+import os
+#os.chdir('/home/juan/Documentos/cesbio/assigments/Code/')
+os.chdir('E:/CESBIO/preparation/rs_assigment/Pytorch/')
