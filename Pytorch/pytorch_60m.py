44349e6c08e797cd6a8a0bd4324d88c136c4c517
JSVinasco <vinasco.juan@correounivalle.edu.co>
2020-06-07 15:26:08 -0500

    60 minutes

--- Pytorch/pytorch_60m.py
+++ Pytorch/pytorch_60m.py
@@ -217,13 +217,13 @@ class Net(nn.Module):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 3x3 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(1,6,3)
-        self.conv2 = nn.Conv2d(6,16,3)
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120) # 6*6 from image dimension
-        self.fc2 = nn.Linear(120 , 84)
-        self.fc3 = nn.Linear(84 , 10)
-    
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        #
     def forward(self, x):
         # Max pooling over a (2, 2) window
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
@@ -234,13 +234,13 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
+        #
     def num_flat_features(self, x):
-        size = x.size()[1:] # all dimensions except the batch dimension
+        size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
             num_features *= s
-            return num_features
+        return num_features
 
 
 net = Net()
@@ -254,7 +254,7 @@ print(len(params))
 print(params[0].size()) # conv1's .weight
 
 input = torch.rand(1, 1, 32, 32)
-input2 = torch.reshape(input,(32,32,1,1))
+#input2 = torch.reshape(input,(32,32,1,1))
 out = net(input)
 print(out)
 
@@ -279,8 +279,8 @@ print(loss)
 
 
 print(loss.grad_fn) #MSELoss
-print(loss.grad_fn.next_functions[0[0]]) # Linear
-print(loss.grad_fn.next_function[0][0].next_function[0][0]) # ReLU
+print(loss.grad_fn.next_functions[0][0]) # Linear
+print(loss.grad_fn.next_functions[0][0].next_functions[0][0]) # ReLU
 
 ########################
 #### Backpropagation ###
@@ -388,7 +388,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-
+        #
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
@@ -409,21 +409,21 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 
 for epoch in range(2):  # loop over the dataset multiple times
-
+    #
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-
+        #
         # zero the parameter gradients
         optimizer.zero_grad()
-
+        #
         # forward + backward + optimize
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-
+        #
         # print statistics
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
